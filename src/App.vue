830148f42006aafb10994949baf17c3788c11c14
Namicici <namicici.lee@outlook.com>
2024-07-17 09:55:24 +0000

    feat: 视频添加字幕

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue'
+
+const videoUrl = ref('/static/IMG_4038.mp4')
+
+function  handleSelectSourceVideo() {
+  let elem = document.createElement('input')
+  elem.setAttribute('type', 'file')
+  elem.addEventListener('change', () => {
+    if (elem.files && elem.files?.length >= 1) {
+      let file = elem.files[0]
+      videoUrl.value = URL.createObjectURL(file)
+    }
+  })
+  elem.click()
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section class="source-section">
+    <section class="source-video">
+      <h1>原视频</h1>
+      <button @click="handleSelectSourceVideo">选择视频</button>
+      <video controls :src="videoUrl">
+      </video>
+    </section>
+    <section class="caption">
+      <h1>字幕</h1>
+    </section>
+  </section>
+  <section class="new-video">
+    <h1>合成视频</h1>
+    <video controls :src="videoUrl">
+      <track kind="subtitles" src="/static/sample.vtt"></track>
+    </video>
+  </section>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style scoped lang="less">
+.source-section {
+  display: flex;
+  justify-content: space-between;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.source-video {
+  width: 50%;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.caption {
+  width: 50%;
+}
+video {
+  margin-top: 8px;
+  display: block;
+  width:100%;
+}
+
+::cue {
+  color: red;
+  background: transparent;
 }
 </style>
